c1c9375fae5b67ff6152ab31a081613d914b0edf
JuanRodriguez95 <juanrodriguezcampos818@gmail.com>
2021-05-17 10:12:05 -0500

    prueba jenkins

--- src/main/java/co/edu/unicundi/proyectoSpringPrueba/ProyectoSpringPruebaApplication.java
+++ src/main/java/co/edu/unicundi/proyectoSpringPrueba/ProyectoSpringPruebaApplication.java
@@ -2,16 +2,37 @@ package co.edu.unicundi.proyectoSpringPrueba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 @SpringBootApplication
-public class ProyectoSpringPruebaApplication {
+public class ProyectoSpringPruebaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoSpringPruebaApplication.class, args);
 	}
 
-	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	return application.sources(ProyectoSpringPruebaApplication.class);
+	}
 }
+
+/*
+@SpringBootApplication
+@EnableScheduling
+public class MuvitappBackendApplication extends SpringBootServletInitializer{
+
+ public static void main(String[] args) {
+SpringApplication.run(MuvitappBackendApplication.class, args);
+}
+@Override
+protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+return application.sources(MuvitappBackendApplication.class);
+}
+
+}
+*/

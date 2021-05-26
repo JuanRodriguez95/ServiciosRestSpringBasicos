6ba05b43d1fa2dbd4aa20b688e8117b53b665b87
JuanRodriguez95 <juanrodriguezcampos818@gmail.com>
2021-05-26 18:23:47 -0500

    commit 26 de mayo2

--- src/main/java/co/edu/unicundi/proyectoSpringPrueba/service/Interface/ICrud2.java
+++
@@ -1,7 +0,0 @@
-package co.edu.unicundi.proyectoSpringPrueba.service.Interface;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ICrud2<T, V> extends JpaRepository<T, V> {
-    
-}

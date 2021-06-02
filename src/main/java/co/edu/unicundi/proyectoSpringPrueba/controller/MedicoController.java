b2ce93e30c0a8c1cb3003d31a18f30aebd558e91
JuanRodriguez95 <juanrodriguezcampos818@gmail.com>
2021-06-02 09:11:02 -0500

    prueba jenkins 2 de junio

--- src/main/java/co/edu/unicundi/proyectoSpringPrueba/controller/MedicoController.java
+++ src/main/java/co/edu/unicundi/proyectoSpringPrueba/controller/MedicoController.java
@@ -113,7 +113,7 @@ public class MedicoController {
     @GetMapping("/pruebaJenkins/{nombre}/{edad}/{ciudad}")
     public ResponseEntity<?> pruebaJenkins(@PathVariable String nombre,@PathVariable Integer edad,@PathVariable String ciudad)
             throws EmptyListException, OrderNotFoundException {
-        String saludo = "HOLA "+ nombre+" Bienvenido; "+"Tienes "+ edad + " años ; y vives en "+ciudad+" ";
+        String saludo = "HOLA "+ nombre+" Bienvenido; "+"Tienes "+ edad + " años ; y vives en "+ciudad+" jajaja prueba";
         return new ResponseEntity<String>(saludo,HttpStatus.OK);
     }
 
